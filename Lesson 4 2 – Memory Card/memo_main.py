be2e0af83bd69b2a8f753aa791d70149c8692294
ddesperaate <137008261+ddesperaate@users.noreply.github.com>
2023-09-28 20:44:06 +0300

    memo_main.py

--- Lesson 4 2 – Memory Card/memo_main.py
+++ Lesson 4 2 – Memory Card/memo_main.py
@@ -1,6 +1,9 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton
 
+# import module second
+from memo_card_layout import *
+
 # crate main objects
 app = QApplication([])
 main_window = QWidget()
@@ -10,8 +13,13 @@ main_window.resize(card_width, card_height)
 main_window.move(50, 50)
 
 
+# while click button
+answer_pushbutton.clicked.connect(show_result)
 
+
+main_window.setLayout(mainvline)
 main_window.show()
 app.exec_()
+
 
 
