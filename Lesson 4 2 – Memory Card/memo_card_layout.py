7ffebd209731448b9cd1040226e83695dc4e4edb
ddesperaate <137008261+ddesperaate@users.noreply.github.com>
2023-09-28 20:43:40 +0300

    memo_card_layout.py

--- Lesson 4 2 – Memory Card/memo_card_layout.py
+++ Lesson 4 2 – Memory Card/memo_card_layout.py
@@ -11,6 +11,9 @@ button2 = QRadioButton("2")
 button3 = QRadioButton("3")
 button4 = QRadioButton("4")
 
+# create main vert line
+mainvline = QVBoxLayout()
+
 # Об'єднання радіобаттонів у одну групу
 RadioGroupBox = QGroupBox("Варіанти відповідей")
 
@@ -58,4 +61,30 @@ RadioGroup.setLayout(horizont_line3)
 horizont_line4 = QHBoxLayout()
 horizont_line4.addWidget(answer_pushbutton)
 
+
+
+# ---------- New Online Lesson 1 ----------
+
+# ----- Box 2 with answers
+box2 = QGroupBox("Результат теста")
+
+label_true_false = QLabel(" idk ")
+answer1 = QLabel(" ")#apple
+
+v_res = QVBoxLayout()
+v_res.addWidget(label_true_false, alignment = (Qt.AlignTop| Qt.AlignLeft))
+v_res.addWidget(answer1, alignment = Qt.AlignCenter, stretch = 2)
+box2.setLayout(v_res)
+
+horizont_line3.addWidget(box2)
+box2.hide()
+
+def show_result(): #while push button - answer_pushbutton
+    box1.hide()
+    box2.show()
+    answer_pushbutton.setText("Наступне питання")
+
+def show_question():
+    RadioGroupBox.show()
+    answer_pushbutton.hide()
 
