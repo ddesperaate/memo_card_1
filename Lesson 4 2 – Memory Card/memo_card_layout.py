fba9fb4318ddaf4010771dd1e3b2dbbb0e13d939
ddesperaate <137008261+ddesperaate@users.noreply.github.com>
2023-10-28 17:12:58 +0300

    Update memo_card_layout.py

--- Lesson 4 2 – Memory Card/memo_card_layout.py
+++ Lesson 4 2 – Memory Card/memo_card_layout.py
@@ -1,97 +1,107 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton, QLineEdit
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication
+app = QApplication([])
 
+menu_pushbutton = QPushButton("Меню")
+pouse_continue_pushbutton = QPushButton("Пауза/Старт")
+answer_pushbutton = QPushButton("Відповідь")
 
-# crate main objects
+button1 = QRadioButton("1")
+button2 = QRadioButton("2")
+button3 = QRadioButton("3")
+button4 = QRadioButton("4")
 
-question_window = QWidget()
-question_window.setWindowTitle("Question Card")
-qcard_width, qcard_height = 600, 350
-question_window.resize(qcard_width, qcard_height)
-question_window.move(150, 150)
+# create main vert line
+mainvline = QVBoxLayout()
 
-# vertical lines
-zero_main_vline = QVBoxLayout()
-one_vline = QVBoxLayout()
-two_vline = QVBoxLayout()
+# Об'єднання радіобаттонів у одну групу
+RadioGroupBox = QGroupBox("Варіанти відповідей")
 
-# horizont lines
-one_hline = QHBoxLayout()
-two_hline = QHBoxLayout()
-three_hline = QHBoxLayout()
+# Рамка для групи перемекичів з відповідями
+RadioGroup = QButtonGroup()
 
-# create labels
-one_label = QLabel("Введіть запитання")
-two_label = QLabel("Введіть вірну відповідь")
-three_label = QLabel("Введіть першу хибну відповідь")
-four_label = QLabel("Введіть другу хибну відповідь")
-five_label = QLabel("Введіть третю хибну відповідь")
+RadioGroup.addButton(button1)
+RadioGroup.addButton(button2)
+RadioGroup.addButton(button3)
+RadioGroup.addButton(button4)
 
-# create QLineEdit
-one_qlineedit = QLineEdit()
-two_qlineedit = QLineEdit()
-three_qlineedit = QLineEdit()
-four_qlineedit = QLineEdit()
-five_qlineedit = QLineEdit()
+#spin_button
+box_minutes = QSpinBox()
+box_minutes.setValue(30)
 
-# create pushbuttons
-add_question_pushbutton = QPushButton("Додати запитання")
-clear_pushbutton = QPushButton("Очистити")
-next_pushbutton = QPushButton("Далі")
+# create two vertical line
+vert_line_left = QVBoxLayout()
+vert_line_right = QVBoxLayout()
 
+# add four radiobutton on two vertical line
+vert_line_left.addWidget(button1)
+vert_line_left.addWidget(button2)
+vert_line_right.addWidget(button3)
+vert_line_right.addWidget(button4)
 
-
-#---------------------------------------------------------
-# add labels to one_hline
-one_vline.addWidget(one_label)
-one_vline.addWidget(two_label)
-one_vline.addWidget(three_label)
-one_vline.addWidget(four_label)
-one_vline.addWidget(five_label)
-
-# add QLineEdits to two_hline
-two_vline.addWidget(one_qlineedit)
-two_vline.addWidget(two_qlineedit)
-two_vline.addWidget(three_qlineedit)
-two_vline.addWidget(four_qlineedit)
-two_vline.addWidget(five_qlineedit)
-
-
-# add one_vline and two_vline to one_hline
-one_hline.addLayout(one_vline)
-one_hline.addLayout(two_vline)
-
-# add pushbuttons to two_hline
-two_hline.addWidget(add_question_pushbutton)
-two_hline.addWidget(clear_pushbutton)
-# add next_pushbutton to third hline
-three_hline.addWidget(next_pushbutton)
-
-# add one_hline and two_hline to zero_main_vline
-zero_main_vline.addLayout(one_hline)
-zero_main_vline.addLayout(two_hline)
-zero_main_vline.addWidget(next_pushbutton)
+dop_hline = QHBoxLayout()
+dop_hline.addLayout(vert_line_left)
+dop_hline.addLayout(vert_line_right)
+RadioGroupBox.setLayout(dop_hline)
 
 
 
 
-class Question():
-    def __init__ (self, question, answer, wrong_answer1, wrong_answer2, wrong_answer3):
-        #конструктор класу
-        self.question = question
-        self.answer = answer
-        self.wrong_answer1 = wrong_answer1
-        self.wrong_answer2 = wrong_answer2
-        self.wrong_answer3 = wrong_answer3
-        self.attempts = 0 # Переменная для всез ответов
-        self.correct = 0 # Переменная для подсчёта правльных ответов
-    
 
-    def got_right(self):
-        print("Це правильна відповідь!")
-        self.attempts += 1
-        self.correct += 1
 
-    def got_wrong(self):
-        print("Відповідь невірна")
-        self.attempts += 1
+
+
+# Horizont line
+horizont_line1 = QHBoxLayout()
+horizont_line1.addWidget(menu_pushbutton)
+horizont_line1.addWidget(pouse_continue_pushbutton)
+horizont_line1.addWidget(box_minutes)
+minutes_left = QLabel("хвилин")
+horizont_line1.addWidget(minutes_left)
+
+# second H line
+horizont_line2 = QHBoxLayout()
+question_widget = QLabel("Apple")
+horizont_line2.addWidget(question_widget)
+
+# third H line
+horizont_line3 = QHBoxLayout()
+horizont_line3.addWidget(RadioGroupBox)
+
+# fourth H line
+horizont_line4 = QHBoxLayout()
+horizont_line4.addWidget(answer_pushbutton)
+
+
+
+# ---------- New Online Lesson 1 ----------
+
+# ----- Box 2 with answers
+box2 = QGroupBox("Результат теста")
+
+label_true_false = QLabel(" idk ")
+answer1 = QLabel(" ")#apple
+
+v_res = QVBoxLayout()
+v_res.addWidget(label_true_false, alignment = (Qt.AlignTop| Qt.AlignLeft))
+v_res.addWidget(answer1, alignment = Qt.AlignCenter, stretch = 2)
+box2.setLayout(v_res)
+
+horizont_line3.addWidget(box2)
+box2.hide()
+
+def show_result(): #while push button - answer_pushbutton
+    RadioGroupBox.hide()
+    box2.show()
+    answer_pushbutton.setText("Наступне питання")
+
+def show_question():
+    RadioGroupBox.show()
+    box2.hide()
+
+# set all widgets on main window
+mainvline.addLayout(horizont_line1)
+mainvline.addLayout(horizont_line2)
+mainvline.addLayout(horizont_line3)
+mainvline.addLayout(horizont_line4)
